506cd7df96c227a4959101e644ccb22bcec0f9d8
Егор Ткаченко <egortkachenco@gmail.com>
2023-11-27 14:50:02 +0200

    initial commit

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: "firebase-storage",
+    providerOptions: {
+      serviceAccount: require("./SERVICE_ACCOUNT_KEY_PATH.json"),
+      bucketUrl: env("STORAGE_BUCKET_URL"),
+      uploadOptions: {},
+      deleteOptions: {},
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c29685cc-6ce7-43ce-b9a6-9380780b888f'
+};
