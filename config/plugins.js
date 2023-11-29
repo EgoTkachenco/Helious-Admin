51dda8afa695e0be024bcee95cc25d102db2e0c4
Егор Ткаченко <egortkachenco@gmail.com>
2023-11-29 19:52:15 +0200

    updates

--- config/plugins.js
+++ config/plugins.js
@@ -3,10 +3,8 @@ module.exports = ({ env }) => ({
   upload: {
     provider: "firebase-storage",
     providerOptions: {
-      serviceAccount: require("./SERVICE_ACCOUNT_KEY_PATH.json"),
-      bucketUrl: env("STORAGE_BUCKET_URL"),
-      uploadOptions: {},
-      deleteOptions: {},
+      serviceAccount: require(env("FIREBASE_SERVICE_ACCOUNT_PATH")),
+      bucketUrl: env("FIREBASE_STORAGE_BUCKET_URL"),
     },
   },
 });
