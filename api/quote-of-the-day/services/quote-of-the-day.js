09e032226d16944d6b87b707510319886dc35706
Егор Ткаченко <egortkachenco@gmail.com>
2023-12-05 16:57:27 +0200

    sql error fix

--- api/quote-of-the-day/services/quote-of-the-day.js
+++ api/quote-of-the-day/services/quote-of-the-day.js
@@ -4,11 +4,9 @@ module.exports = {
   async updateRandomQuote() {
     const sql = `SELECT * FROM quotes ORDER BY RAND() LIMIT 1;`;
     let result = await strapi.connections.default.raw(sql);
-    if (!result[0]) return;
-    const quote = result[0][0];
-    console.log(quote.id);
-    if (!quote) return;
+    if (!result[0] || !result[0][0]) return;
 
+    const quote = result[0][0];
     const quote_of_the_day = await this.find({}, []);
     if (quote.id !== quote_of_the_day.quote) {
       await this.createOrUpdate({ quote: quote.id });
