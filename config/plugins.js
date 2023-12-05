dc24e4710b0d56e1859dabfd58a06267b051385d
Егор Ткаченко <egortkachenco@gmail.com>
2023-12-05 16:36:10 +0200

    change firebase storage to local folder

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
   // ...
-  upload: {
-    provider: "firebase-storage",
-    providerOptions: {
-      serviceAccount: require(env("FIREBASE_SERVICE_ACCOUNT_PATH")),
-      bucketUrl: env("FIREBASE_STORAGE_BUCKET_URL"),
-    },
-  },
+  // upload: {
+  //   provider: "firebase-storage",
+  //   providerOptions: {
+  //     serviceAccount: require(env("FIREBASE_SERVICE_ACCOUNT_PATH")),
+  //     bucketUrl: env("FIREBASE_STORAGE_BUCKET_URL"),
+  //   },
+  // },
 });
